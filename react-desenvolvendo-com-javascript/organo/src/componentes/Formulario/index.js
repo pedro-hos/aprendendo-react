9cbe345119e6719774d3fc945f335281f5d22ee7
Pedro Hos <pedro-hos@outlook.com>
2023-04-24 20:14:52 -0300

    aulas

---
+++ react-desenvolvendo-com-javascript/organo/src/componentes/Formulario/index.js
@@ -0,0 +1,36 @@
+import "./Formulario.css";
+
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
+
+const times = [
+  "Programação",
+  "Front-End",
+  "Data Science",
+  "Devops",
+  "UX e Design",
+  "Mobile",
+  "Inovação e Gestão",
+];
+
+const aoSalvar = (event) => {
+  event.preventDefault();
+  console.log("Form foi submetido");
+};
+
+const Formulario = () => {
+  return (
+    <section className="formulario">
+      <form onSubmit={aoSalvar}>
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa obrigatorio={true} label="Times" itens={times} />
+        <Botao>Criar Card</Botao>
+      </form>
+    </section>
+  );
+};
+
+export default Formulario;
