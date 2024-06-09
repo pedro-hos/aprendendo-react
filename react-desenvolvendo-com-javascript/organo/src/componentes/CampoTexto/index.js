fcdf895ccc7e9b92f7e4ea8ddb7c3ae6cd1c82d5
Pedro Hos <pedro-hos@outlook.com>
2024-06-09 15:24:09 -0300

    ReactJS + TypeScript

--- react-desenvolvendo-com-javascript/organo/src/componentes/CampoTexto/index.js
+++ react-desenvolvendo-com-javascript/organo/src/componentes/CampoTexto/index.js
@@ -1,13 +1,21 @@
-import './CampoTexto.css'
+import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value);
+  };
 
-    return(
-        <div className='campo-texto'>
-            <label>{props.label}</label>
-            <input required={props.obrigatorio} type="text" placeholder={props.placeholder}/>
-        </div>
-    )
-}
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input
+        value={props.valor} onChange={aoDigitado} 
+        required={props.obrigatorio}
+        type="text"
+        placeholder={props.placeholder}
+      />
+    </div>
+  );
+};
 
 export default CampoTexto;
--- react-desenvolvendo-com-javascript/organo/src/componentes/Formulario/index.js
+++ react-desenvolvendo-com-javascript/organo/src/componentes/Formulario/index.js
@@ -3,6 +3,7 @@ import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
+import { useState } from "react";
 
 const times = [
   "Programação",
@@ -14,19 +15,54 @@ const times = [
   "Inovação e Gestão",
 ];
 
-const aoSalvar = (event) => {
-  event.preventDefault();
-  console.log("Form foi submetido");
-};
+const Formulario = (props) => {
+  const aoSalvar = (event) => {
+    event.preventDefault();
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time,
+    });
+  };
 
-const Formulario = () => {
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
+
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
-        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa obrigatorio={true} label="Times" itens={times} />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Digite o endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
+
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Times"
+          itens={times}
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+        />
+
         <Botao>Criar Card</Botao>
       </form>
     </section>
